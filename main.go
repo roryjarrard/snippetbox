e6d7b98d7d14b1c6e9302fdb038ead5e967ac234
Rory Jarrard <rjarrard@frontsteps.com>
2023-03-15 13:58:35 -0600

    restrict create to post

--- main.go
+++ main.go
@@ -18,6 +18,12 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
